49be0c3342603cca6815e1b6e269eb72ba5de568
mwcproject <vsts@fv-az95-371.3v5gt4fovkquvddej2ilmqduwg.ex.internal.cloudapp.net>
2021-05-04 20:13:11 +0000

    Pipelines-Bot: Updated site via 0d7fbe46b7cd8ae5072ab32f60d50e428b73291b

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["grin_api"] = {"name":"","dirs":[{"name":"handlers","files":["blocks_api.rs","chain_api.rs","peers_api.rs","pool_api.rs","server_api.rs","transactions_api.rs","utils.rs","version_api.rs"]}],"files":["auth.rs","client.rs","foreign.rs","foreign_rpc.rs","handlers.rs","lib.rs","owner.rs","owner_rpc.rs","rest.rs","router.rs","stratum.rs","stratum_rpc.rs","types.rs","web.rs"]};
+sourcesIndex["grin_wallet_api"] = {"name":"","files":["foreign.rs","foreign_rpc.rs","lib.rs","owner.rs","owner_rpc_v2.rs","owner_rpc_v3.rs","types.rs"]};
 createSourceSidebar();
