002073ff563cfa639c7649d428fb23ee07f8d11e
mwcproject <vsts@fv-az180-9.2src2xt3ys5erhusjlposv41dg.bx.internal.cloudapp.net>
2021-05-06 20:05:46 +0000

    Pipelines-Bot: Updated site via 478220d4fbefcf16bf89f37742bc845bb537b564

---
+++ Node/implementors/easy_jsonrpc_mw/trait.Handler.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["grin_api"] = [{"text":"impl Handler for dyn <a class=\"trait\" href=\"grin_api/trait.ForeignRpc.html\" title=\"trait grin_api::ForeignRpc\">ForeignRpc</a>","synthetic":false,"types":["grin_api::foreign_rpc::ForeignRpc"]},{"text":"impl Handler for dyn <a class=\"trait\" href=\"grin_api/trait.OwnerRpc.html\" title=\"trait grin_api::OwnerRpc\">OwnerRpc</a>","synthetic":false,"types":["grin_api::owner_rpc::OwnerRpc"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ Node/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["grin_api"] = {"name":"","dirs":[{"name":"handlers","files":["blocks_api.rs","chain_api.rs","peers_api.rs","pool_api.rs","server_api.rs","transactions_api.rs","utils.rs","version_api.rs"]}],"files":["auth.rs","client.rs","foreign.rs","foreign_rpc.rs","handlers.rs","lib.rs","owner.rs","owner_rpc.rs","rest.rs","router.rs","stratum.rs","stratum_rpc.rs","types.rs","web.rs"]};
+createSourceSidebar();
